c57d86725b9a1a97ebede8282b97f794b3e6f22a
riyaverma85 <riyaverma2976@gmail.com>
2025-07-31 23:34:59 -0700

    insert

--- frontend/src/pages/Insert.jsx
+++ frontend/src/pages/Insert.jsx
@@ -4,6 +4,13 @@ const Insert=()=>{
     return(
         <>
          <h3 className="h33"> Insert Student Record page!!!</h3>
+        <form action="/save" method="post" id="form">
+        Enter Name: <input type="text" name="name"/><br/><br/>
+        Enter Subject: <input type="text" name="sub"/><br/><br/>
+        Enter City: <input type="text" name="city"/><br/><br/>
+        Enter Fees: <input type="text" name="fees"/><br/><br/>
+        <button>Save</button>
+    </form>
         </>
     )
 }
