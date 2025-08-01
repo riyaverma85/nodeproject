f90b655474fef7d6451b328ca60cec4d7efdd66a
riyaverma85 <riyaverma2976@gmail.com>
2025-07-31 23:26:09 -0700

    css

--- frontend/src/components/Topnav.jsx
+++ frontend/src/components/Topnav.jsx
@@ -8,14 +8,14 @@ const Topnav=()=>{
         <>
         <Navbar id="top">
         <Container>
-          <Navbar.Brand href="#home">Crud Opration</Navbar.Brand>
+          <Navbar.Brand href="#home" id="title">CRUD OPRATION</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="home">Home</Nav.Link>
-            <Nav.Link as={Link} to="insert">Insert</Nav.Link>
-            <Nav.Link as={Link} to="display">Display</Nav.Link>
-            <Nav.Link as={Link} to="update">Update</Nav.Link>
-            <Nav.Link as={Link} to="search">Search</Nav.Link>
-            <Nav.Link as={Link} to="contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="home" className='head'>Home</Nav.Link>
+            <Nav.Link as={Link} to="insert" className='head'>Insert</Nav.Link>
+            <Nav.Link as={Link} to="display" className='head'>Display</Nav.Link>
+            <Nav.Link as={Link} to="update" className='head'>Update</Nav.Link>
+            <Nav.Link as={Link} to="search" className='head'>Search</Nav.Link>
+            <Nav.Link as={Link} to="contact" className='head'>Contact</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
