0cd1d1393870265c428c6263ebce190d39487269
riyaverma85 <riyaverma2976@gmail.com>
2025-08-01 01:15:10 -0700

    css

--- frontend/src/pages/Display.jsx
+++ frontend/src/pages/Display.jsx
@@ -1,9 +1,22 @@
 
-
+import Table from 'react-bootstrap/Table';
 const Display=()=>{
     return(
         <>
          <h3 className="h33">Student Data display page!!!</h3>
+         <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Username</th>
+        </tr>
+      </thead>
+      <tbody>
+
+      </tbody>
+      </Table>
         </>
     )
 }
