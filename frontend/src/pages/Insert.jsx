5169371b682ff9d8d4089aceb7f90337edf8669d
riyaverma85 <riyaverma2976@gmail.com>
2025-08-01 23:35:05 -0700

    form

--- frontend/src/pages/Insert.jsx
+++ frontend/src/pages/Insert.jsx
@@ -29,22 +29,25 @@ const Insert=()=>{
         <button onClick={handleSubmit}>Save</button>
     </form> */}
 
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <Form id="form">
+      <Form.Group className="mb-3" >
+        <Form.Label>Enter Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter email" name="name" onChange={handleInput}/>
+     </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+      <Form.Group className="mb-3" >
+        <Form.Label>Enter Rollno</Form.Label>
+        <Form.Control type="text" placeholder="Password" name="rollno" onChange={handleInput}/>
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+      <Form.Group className="mb-3">
+        <Form.Label>Enter City</Form.Label>
+        <Form.Control type="text" placeholder="Password" name="city" onChange={handleInput}/>
       </Form.Group>
+      <Form.Group className="mb-3" >
+        <Form.Label>Enter Fees</Form.Label>
+        <Form.Control type="text" placeholder="Password" name="fees" onChange={handleInput}/>
+      </Form.Group>
+      
       <Button variant="primary" type="submit">
         Submit
       </Button>
