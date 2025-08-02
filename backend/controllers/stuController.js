330f64198fba0c569743e99bc3214f0056bc1da9
riyaverma85 <riyaverma2976@gmail.com>
2025-08-01 18:26:48 -0700

    data

--- backend/controllers/stuController.js
+++ backend/controllers/stuController.js
@@ -1,6 +1,9 @@
 
+const stuModel=require("../models/stuModel");
 const stuSave=async(req,res)=>{
     
+     console.log(req.body)
+     res.send("okkk");
 }
 
 module.exports={
--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,13 @@
 const express=require("express");
 const app=express();
 const stuRoute=require("./routes/stuRoutes");
+const mongoose=require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/shivdb").then(()=>{
+    console.log("Database connected successfully");
+})
+    
 
-app.use("/students",statusbar);
+app.use("/students",stuRoute);
 
 app.listen(3000,()=>{
     console.log("Server is running on port 3000");
