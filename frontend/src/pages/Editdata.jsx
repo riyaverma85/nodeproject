ecf9cb3bf3feff9868c882f8f79783d50c4eadae
riyaverma85 <riyaverma2976@gmail.com>
2025-08-05 00:53:26 -0700

    css

--- frontend/src/pages/Editdata.jsx
+++ frontend/src/pages/Editdata.jsx
@@ -1,7 +1,18 @@
 
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import BackendURl from "../utils/BackendURL";
 const Editdata=()=>{
     const {id}=useParams();
+    const[mydata,setMydata]=useState({});
+    const loadData=async()=>{
+        let api=`${BackendURl}students/edit/${id}`;
+        const response=await get(api);
+        setMydata(response.data)
+    }
+    useEffect(()=>{
+        loadData();
+    },[])
     return(
         <>
         <h3>Edit Data!!! {id}</h3>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,14 @@ const Home=()=>{
     return(
         <>
          <h3 className="h33">Welcome to our home page!!!</h3>
+         <div className="home">
+            <div className="homebox">
+                  <h1>Welcome to My Website!</h1>
+                  <p>This is a simple Node.js-powered website homepage.</p>
+                  <p>Explore features, learn about our services, or contact us for more info.</p>
+            </div>
+
+         </div>
         </>
     )
 }
