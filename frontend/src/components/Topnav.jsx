96be728ace992f4497235d1637bb4c251e675364
riyaverma85 <riyaverma2976@gmail.com>
2025-08-07 01:17:41 -0700

    icon

--- frontend/src/components/Topnav.jsx
+++ frontend/src/components/Topnav.jsx
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
+import { FaBars } from "react-icons/fa";
+
 const Topnav=()=>{
     return(
         <>
