5daf5ce03082fd8221b10e8b02b63e21e0e86935
riyaverma85 <riyaverma2976@gmail.com>
2025-08-06 18:08:35 -0700

    search

--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -11,14 +11,14 @@ const Search=()=>{
         e.preventDefault();
       let api=`${BackendURl}students/search`;
       const response=await axios.post(api,{rollno:rno});
-      console,log(response.data);
+      console.log(response.data);
       setMydata(response.data);
     }
     
     return(
         <>
          <h3 className="h33">Search Data page!!!</h3>
-        <Form id="form">
+        <Form id="form2">
       <Form.Group className="mb-3" >
         <Form.Label>Enter Name</Form.Label>
         <Form.Control type="text" name="name" value={rno} onChange={(e)=>{setRno(e.target.value)}}/>
@@ -28,10 +28,10 @@ const Search=()=>{
            </Button>
          </Form>
 
-         <Table striped bordered hover variant="dark">
+         <Table striped bordered hover variant="dark" className='tbl'>
                <thead>
                  <tr>
-                   <th>Sno</th>
+                   
                    <th>Name</th>
                    <th>Rollno</th>
                    <th>City</th>
