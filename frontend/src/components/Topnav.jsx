eda8a57175da878782e3f27609f2a0757f94d7bd
riyaverma85 <riyaverma2976@gmail.com>
2025-08-07 01:24:31 -0700

    nav

--- frontend/src/components/Topnav.jsx
+++ frontend/src/components/Topnav.jsx
@@ -8,9 +8,11 @@ import { FaBars } from "react-icons/fa";
 const Topnav=()=>{
     return(
         <>
-        <Navbar id="top">
+        <Navbar id="top" bg="dark" variant="dark" expand={false} collapseOnSelect>
         <Container>
           <Navbar.Brand href="#home" id="title">CRUD OPRATION</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="home" className='head'>Home</Nav.Link>
             <Nav.Link as={Link} to="insert" className='head'>Insert</Nav.Link>
@@ -19,8 +21,13 @@ const Topnav=()=>{
             <Nav.Link as={Link} to="search" className='head'>Search</Nav.Link>
             <Nav.Link as={Link} to="contact" className='head'>Contact</Nav.Link>
         </Nav>
+         </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+        <FaBars size={22} color="white" />
+      </div>
         </>
     )
 }
